11facdba0f7f53e3f8ae971cd048aa5f15f77538
rexcare367 <rex.care.367@gmail.com>
2023-12-26 14:08:18 +0300

    create project

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar
+        app
+        color="white"
+        flat
+      >
+        <v-container class="py-0">
+          <img
+            class="mr-10"
+            style="width: 40px;height: 40px;cursor: pointer"
+            src="/LinkedIn_icon.svg"
+          >
+        </v-container>
+      </v-app-bar>
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container />
+</template>
+
+<script>
+export default {
+
+}
+</script>
