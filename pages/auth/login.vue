78a14d015b23bd0528bcb2855bed921a002403fd
rexcare367 <rex.care.367@gmail.com>
2024-03-20 23:40:26 +0300

    changed login

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,24 +1,64 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="6">
-        <v-card>
-          <v-card-title class="text-center">
-            Giriş Yap
-          </v-card-title>
-          <v-card-text>
-            <v-form @submit.prevent="login">
-              <v-text-field v-model="login_Info.email" label="E-posta" required />
-              <v-text-field v-model="login_Info.password" label="Şifre" type="password" required />
-              <v-btn type="submit" color="primary">
-                Giriş Yap
+  <v-app>
+    <v-container class="align-center justify-center align-content-center text-center d-flex">
+      <v-card elevation="0" class="mt-15">
+        <v-card width="352">
+          <v-card-title>Oturum Aç</v-card-title>
+          <v-card-subtitle>
+            Profesyonel dünyanızla ilgili güncel haberlere sahip olun
+          </v-card-subtitle>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="login_Info.email"
+              class="mx-4"
+              outlined
+              :rules="emailRules"
+              label="E-posta"
+              background-color="white"
+              required
+            />
+            <v-text-field
+              v-model="login_Info.password"
+              type="password"
+              class="mx-4"
+              outlined
+              :rules="passwordRules"
+              label="Şifre"
+              required
+              background-color="white"
+            />
+          </v-form>
+
+          <v-card-actions>
+            <div class="ml-5 mb-4" style="width: calc(100% - 50px)">
+              <v-btn
+                block
+                style="border-radius: 500px;color: white;background-color: #0A66C2"
+                height="46"
+                elevation="0"
+                :disabled="!valid"
+                @click="login"
+              >
+                Oturum Açın
               </v-btn>
-            </v-form>
-          </v-card-text>
+            </div>
+          </v-card-actions>
         </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+        LinkedIn‘de yeni misiniz?
+        <v-btn
+          text
+
+          @click="redirectRegister"
+        >
+          Hemen katılın
+        </v-btn>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -26,26 +66,37 @@ export default {
   layout: 'auth',
   data () {
     return {
+      valid: true,
       login_Info: {
         email: '',
         password: ''
-      }
+      },
+      emailRules: [
+        v => !!v || 'E-posta adresi zorunludur!',
+        v => /.+@.+\..+/.test(v) || 'Geçerli bir e-posta adresi giriniz'
+      ],
+      passwordRules: [
+        v => !!v || 'Şifrenizi girmek zorunludur!'
+      ]
     }
   },
   methods: {
     login () {
-      try {
-        this.$auth.loginWith('local', { data: this.login_Info })
-          .then(() => {
-            this.$router.push('/')
-          })
-      } catch (error) { // çalıştıramadım kontrol edilecek burası
-        if (error.response && error.response.status === 401) {
-          alert('Kullanıcı adı veya şifre yanlış.')
-        } else {
-          alert('Giriş işlemi sırasında bir hata oluştu.')
-        }
-      }
+      this.$refs.form.validate()
+      this.$auth.loginWith('local', { data: this.login_Info })
+        .then(() => {
+          this.$router.push('/')
+        })
+        .catch((error) => {
+          if (error.message === 'Request failed with status code 401') {
+            alert('E-posta ya da şifreniz hatalı')
+          } else {
+            alert('Giriş bilgilerinizi kontrol ediniz')
+          }
+        })
+    },
+    redirectRegister () {
+      this.$router.push('/auth/register')
     }
   }
 }
